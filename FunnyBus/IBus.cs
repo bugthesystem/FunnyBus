b6a1bce388f7f8fbff8c49543af15fe9492ca89c
Ömer Cinbat <ocinbat@outlook.com>
2016-06-12 19:31:02 +0300

    Autopublish test.

--- FunnyBus/IBus.cs
+++ FunnyBus/IBus.cs
@@ -5,16 +5,21 @@ namespace FunnyBus
     public interface IBus
     {
         void Subscribe<THandler>(THandler handler) where THandler : class;
+
         void Subscribe<THandler>() where THandler : class;
+
         void Subscribe<TMessage>(Action<TMessage> handler) where TMessage : class;
 
         void UnSubscribe<T>() where T : class;
+
         void UnSubscribe<THandler>(THandler handler) where THandler : class;
 
         TResult Publish<TMessage, TResult>(TMessage message) where TMessage : class;
+
         void Publish<TMessage>(TMessage message) where TMessage : class;
 
         TResult Publish<TResult>(object message);
+
         void Publish(object message);
     }
 }
